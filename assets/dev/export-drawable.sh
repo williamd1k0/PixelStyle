4bae6fe19fbeec959472deb2abb6559186610bb3
William Tumeo <tumeowilliam@gmail.com>
2020-02-29 02:48:09 -0300

    Add fallback for unavailable icons

--- assets/dev/export-drawable.sh
+++ assets/dev/export-drawable.sh
@@ -4,3 +4,10 @@ cd ../.resized
 for icon in *.png; do
     ln -srf $icon ../../app/src/main/res/drawable-nodpi/$icon
 done
+
+# Fallback for unavailable upstream icons
+for i in $(find ".png" ../../app/src/main/res/drawable-nodpi/); do
+    if [ ! -L $i ]; then
+        ln -srf template.png $i
+    fi
+done
